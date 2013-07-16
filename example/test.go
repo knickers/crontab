68e8046de3f685f9d4f7b0a80ac81c1237d0afcb
Nick Cox <nickcox1008@gmail.com>
2013-07-15 19:15:19 -0600

    adding the initial files

---
+++ example/test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"crontab"
+)
+
+func main() {
+	fmt.Println("Hi")
+
+	cron := crontab.New()
+
+	err := cron.Load("test.cron")
+	if err != nil {
+		fmt.Println("Cron.Load", err.Error())
+	}
+
+	err = cron.AddString("* * * * * ls -al ~/")
+	if err != nil {
+		fmt.Println("Cron.AddString", err.Error())
+	}
+
+	for i, e := range cron.Jobs {
+		fmt.Printf("%d) %s\n", i, e)
+	}
+
+	err = cron.Save("test.cron")
+	if err != nil {
+		fmt.Println("Cron.Save", err.Error())
+	}
+
+	fmt.Println("Bye")
+}
